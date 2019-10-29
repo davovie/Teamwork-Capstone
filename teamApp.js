0babe6306a1ab642bc61d950d7738eab07ee8837
Ozi Oviezibe <davovie@gmail.com>
2019-10-29 02:02:28 +0100

    Added 2 routes to teamApp.js

--- teamApp.js
+++ teamApp.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -9,7 +10,23 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res) => {
+app.use(bodyParser.json());
+
+// admin can create an employee user account
+app.post('/auth/create-user', (req, res, next) => {
+    res.status(201).json({
+        message: "User account successfully created!"
+    })
+});
+
+// Admin/Employee can login a user
+app.post('/auth/signin', (req, res, next) => {
+    res.status(200).json({
+        message: "Logged in successfully!"
+    })
+});
+
+app.get('/', (req, res) => {
     res.json({message: 'Server starts successfully!'})
 });
 
