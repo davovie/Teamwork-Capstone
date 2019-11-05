5d316b0d3cf1f19b9dd61ab77a7edf86fe66606b
Ozi Oviezibe <davovie@gmail.com>
2019-11-05 18:51:44 +0100

    added new dummy data to test editting article

--- test/data.js
+++ test/data.js
@@ -35,8 +35,14 @@ exports.testGif = {
   date: moment()
 };
 
-exports.testArticle = {
+exports.defaultArticle = {
   title: "A day in development training",
   article:
     "As always, a day starts up with waking from sleep (already sweating) and wondering how to go about fixing the bug I left before falling asleep at the keys (again!!!)"
+};
+
+exports.editedArticle = {
+  title: "A Day In Training @ DEVC Training with Andela",
+  article:
+    "As it always begins, the day starts up with thought of code (already sweating) and wondering how to fix a bug, improve a feature on code snippet left before falling asleep at the keys (sigh!!!)"
 };
